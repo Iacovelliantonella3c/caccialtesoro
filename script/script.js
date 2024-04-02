07ce584889675a56e7354e5de97dc6a0d6bb440d
Iacovelliantonella3c <162125347+Iacovelliantonella3c@users.noreply.github.com>
2024-04-02 17:59:00 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
 
 
-console.log("messaggio");
+function doLogin () {
+  var username = document.getElementById("username").value;
+  var psw = document.getElementById("password").value;
+  if(username == "" || psw == "") { 
+       alert("username e password obbligatori");
+  }
+)
