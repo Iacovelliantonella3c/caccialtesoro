f8c0455336e91a8c5103aded1c404e6d73c440be
Iacovelliantonella3c <162125347+Iacovelliantonella3c@users.noreply.github.com>
2024-04-09 12:36:38 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,4 +6,22 @@ function doLogin () {
   if(username == "" || psw == "") { 
        alert("username e password obbligatori");
   }
+  else {
+
+    //chiamiamo il server per effettuare la verifica 
+    fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad',{
+      method: 'POST',// o 'GET'
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body:JSON.stringify({
+        "username":username,
+        "password":password
+      })
+    })
+    .then(response => response.json()) // converte la risposta in JSON
+    .then(data => console.log(data)) // stampai dati
+    .catch((error) => {
+      console.error('Errore:',error);
+    });
 )
